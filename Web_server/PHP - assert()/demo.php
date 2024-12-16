0827a15cb683c8b46f45eb366cc9eba44f7f28a3
s1mple love <mynameduongnguyen@gmail.com>
2024-12-17 00:55:35 +0700

    add thêmthêm

---
+++ Web_server/PHP - assert()/demo.php
@@ -0,0 +1,9 @@
+<?php
+  if(isset($_GET['page'])){
+    $page = $_GET['page'];
+  }else {
+    $page = "home";
+  }
+  $file = "includes/" . $page ."php";
+  assert("strpos('$file', '..') === false") or die("ERROR: hacking attempt!");
+?>
